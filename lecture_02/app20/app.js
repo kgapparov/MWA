f88570f0aaff2d5d1a667f9a1844db4db8d87a3c
hasangaly <hasangaly@gmail.com>
2022-03-30 07:50:14 -0500

    revew

--- lecture_02/app20/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.set("port", 3000);
-
-const server = app.listen(app.get("port"), function(){
-    console.log("Listening to port" + server.address().port);
-});
--- lecture_02/mean_game/app.js
+++
@@ -1,4 +0,0 @@
-const app = require("express");
-const path = require("path");
-
-app.use("/public", express.static(path.join(__dirname, "public")));
